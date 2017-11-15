070918c7a1c12d15c9c5896ef861d24639d33a64
SaibotlineLv2 <33107731+saibotlv2@users.noreply.github.com>
2017-11-15 10:58:49 +0700

    Update sai.py

--- sai.py
+++ sai.py
@@ -1128,21 +1128,32 @@ def bot(op):
                         except:
                             cl.sendText(msg.to,"Error")
                     
-            elif "Tagall" in msg.text:
-                group = cl.getGroup(msg.to)
-                k = len(group.members)//500
-                for j in xrange(k+1):
-                    msg = Message(to=msg.to)
-                    txt = u''
-                    s=0
-                    d=[]
-                    for i in group.members[j*500 : (j+1)*500]:
-                        d.append({"S":str(s), "E" :str(s+8), "M":i.mid})
-                        s += 9
-                        txt += u'@Krampus\n'
-                    msg.text = txt
-                    msg.contentMetadata = {u'MENTION':json.dumps({"MENTIONEES":d})}
-                    cl.sendMessage(msg) 
+            elif msg.text in ["Tagall"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
 
             elif "Ratakan" in msg.text:
                   if msg.from_ in admin:
